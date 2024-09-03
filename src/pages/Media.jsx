02c5955c3e2b67ca1561b72f3d57658b2d7602bc
IgorLipskiy <igoryurievich1@gmail.com>
2024-09-03 23:03:34 +0200

    Повторная интеграция модального окна только для реакта

--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './adition.css';
+
 const Media = () => {
+  const [currentIndex, setCurrentIndex] = useState(null);
+  const images = useRef([]);
+
+  const openModal = (index) => {
+    setCurrentIndex(index);
+    const modal = new window.bootstrap.Modal(document.getElementById('photoModal'));
+    modal.show();
+  };
+
+  const showPrevImage = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
+  const showNextImage = () => {
+    if (currentIndex < images.current.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
   return (
     <div className="corpo">
       <div className="container">
-      <h1 id="titull" className="container pt-4 text-center">Видео и фотографии</h1>
+        <h1 id="titull" className="container pt-4 text-center">Видео и фотографии</h1>
         <p className="container py-4" style={{ textAlign: 'justify' }} id="zagolovok">
           Приветствуем вас на нашей странице с фотографиями и видео! Здесь вы найдете яркие моменты с наших богослужений, встреч и других мероприятий.
         </p>
+      </div>
+
+      <div className="modal fade" id="photoModal" tabIndex="-1" aria-labelledby="photoModalLabel" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-centered modal-lg-custom">
+          <div className="modal-content">
+            <div className="modal-body">
+              <img id="modalImage" src={currentIndex !== null ? images.current[currentIndex].src : ''} className="img-fluid" alt="Modal" />
+            </div>
+            <div className="modal-footer justify-content-between">
+              <button type="button" className="btn btn-secondary" onClick={showPrevImage} disabled={currentIndex === 0}>
+                <i className="fas fa-chevron-left"></i>
+              </button>
+              <span>{currentIndex !== null ? `Фото ${currentIndex + 1} из ${images.current.length}` : ''}</span>
+              <button type="button" className="btn btn-secondary" onClick={showNextImage} disabled={currentIndex === images.current.length - 1}>
+                <i className="fas fa-chevron-right"></i>
+              </button>
+              <a id="saveImage" className="btn btn-primary" href={currentIndex !== null ? images.current[currentIndex].src : ''} download>
+                <i className="fas fa-download"></i>
+              </a>
+              <button type="button" className="btn btn-danger" data-bs-dismiss="modal">
+                <i className="fas fa-times"></i>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="container py-4">
@@ -54,22 +101,40 @@ const Media = () => {
               <div className="accordion-body">
                 <div className="row flex-wrap">
                   <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/media1.jpg" className="img-fluid" alt="Service" data-bs-image="./images/service1.jpg" />
+                    <img
+                      src="./images/media1.jpg"
+                      className="img-fluid"
+                      alt="Service"
+                      onClick={() => openModal(0)}
+                      ref={(el) => (images.current[0] = el)}
+                    />
                   </div>
                   <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/media2.jpg" className="img-fluid" alt="Service" data-bs-image="./images/service2.jpg" />
+                    <img
+                      src="./images/media2.jpg"
+                      className="img-fluid"
+                      alt="Service"
+                      onClick={() => openModal(1)}
+                      ref={(el) => (images.current[1] = el)}
+                    />
                   </div>
                   <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/media3.jpg" className="img-fluid" alt="Service" data-bs-image="./images/service2.jpg" />
+                    <img
+                      src="./images/media3.jpg"
+                      className="img-fluid"
+                      alt="Service"
+                      onClick={() => openModal(2)}
+                      ref={(el) => (images.current[2] = el)}
+                    />
                   </div>
                   <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/media4.jpg" className="img-fluid" alt="Service" data-bs-image="./images/service2.jpg" />
-                  </div>
-                  <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/media109.jpg" className="img-fluid" alt="Service" data-bs-image="./images/service2.jpg" />
-                  </div>
-                  <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/media134.jpg" className="img-fluid" alt="Service" data-bs-image="./images/service2.jpg" />
+                    <img
+                      src="./images/media4.jpg"
+                      className="img-fluid"
+                      alt="Service"
+                      onClick={() => openModal(3)}
+                      ref={(el) => (images.current[3] = el)}
+                    />
                   </div>
                 </div>
               </div>
@@ -86,40 +151,58 @@ const Media = () => {
               <div className="accordion-body">
                 <div className="row flex-wrap">
                   <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/images_baptism/baptism202301.jpg" className="img-fluid" alt="Baptism" data-bs-image="./images/baptism1.jpg" />
+                    <img
+                      src="./images/images_baptism/baptism202301.jpg"
+                      className="img-fluid"
+                      alt="Baptism"
+                      onClick={() => openModal(4)}
+                      ref={(el) => (images.current[4] = el)}
+                    />
                   </div>
                   <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/images_baptism/baptism202302.jpg" className="img-fluid" alt="Baptism" data-bs-image="./images/baptism2.jpg" />
+                    <img
+                      src="./images/images_baptism/baptism202302.jpg"
+                      className="img-fluid"
+                      alt="Baptism"
+                      onClick={() => openModal(5)}
+                      ref={(el) => (images.current[5] = el)}
+                    />
                   </div>
                   <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/images_baptism/baptism202303.jpg" className="img-fluid" alt="Baptism" data-bs-image="./images/baptism2.jpg" />
+                    <img
+                      src="./images/images_baptism/baptism202303.jpg"
+                      className="img-fluid"
+                      alt="Baptism"
+                      onClick={() => openModal(6)}
+                      ref={(el) => (images.current[6] = el)}
+                    />
                   </div>
                   <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/images_baptism/baptism202304.jpg" className="img-fluid" alt="Baptism" data-bs-image="./images/baptism2.jpg" />
+                    <img
+                      src="./images/images_baptism/baptism202304.jpg"
+                      className="img-fluid"
+                      alt="Baptism"
+                      onClick={() => openModal(7)}
+                      ref={(el) => (images.current[7] = el)}
+                    />
                   </div>
                   <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/images_baptism/baptism202305.jpg" className="img-fluid" alt="Baptism" data-bs-image="./images/baptism2.jpg" />
+                    <img
+                      src="./images/images_baptism/baptism202305.jpg"
+                      className="img-fluid"
+                      alt="Baptism"
+                      onClick={() => openModal(8)}
+                      ref={(el) => (images.current[8] = el)}
+                    />
                   </div>
                   <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/images_baptism/baptism202306.jpg" className="img-fluid" alt="Baptism" data-bs-image="./images/baptism2.jpg" />
-                  </div>
-                  <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/images_baptism/baptism202307.jpg" className="img-fluid" alt="Baptism" data-bs-image="./images/baptism2.jpg" />
-                  </div>
-                  <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/images_baptism/baptism202308.jpg" className="img-fluid" alt="Baptism" data-bs-image="./images/baptism2.jpg" />
-                  </div>
-                  <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/images_baptism/baptism202309.jpg" className="img-fluid" alt="Baptism" data-bs-image="./images/baptism2.jpg" />
-                  </div>
-                  <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/images_baptism/baptism202310.jpg" className="img-fluid" alt="Baptism" data-bs-image="./images/baptism2.jpg" />
-                  </div>
-                  <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/images_baptism/baptism202311.jpg" className="img-fluid" alt="Baptism" data-bs-image="./images/baptism2.jpg" />
-                  </div>
-                  <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/images_baptism/baptism202312.jpg" className="img-fluid" alt="Baptism" data-bs-image="./images/baptism2.jpg" />
+                    <img
+                      src="./images/images_baptism/baptism202306.jpg"
+                      className="img-fluid"
+                      alt="Baptism"
+                      onClick={() => openModal(9)}
+                      ref={(el) => (images.current[9] = el)}
+                    />
                   </div>
                 </div>
               </div>
@@ -136,32 +219,32 @@ const Media = () => {
               <div className="accordion-body">
                 <div className="row flex-wrap">
                   <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/images_baptism/baptism1.jpg" className="img-fluid" alt="Baptism" data-bs-image="./images/baptism1.jpg" />
+                    <img
+                      src="./images/images_baptism/baptism1.jpg"
+                      className="img-fluid"
+                      alt="Baptism"
+                      onClick={() => openModal(10)}
+                      ref={(el) => (images.current[10] = el)}
+                    />
                   </div>
                   <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/images_baptism/baptism2.jpg" className="img-fluid" alt="Baptism" data-bs-image="./images/baptism2.jpg" />
+                    <img
+                      src="./images/images_baptism/baptism2.jpg"
+                      className="img-fluid"
+                      alt="Baptism"
+                      onClick={() => openModal(11)}
+                      ref={(el) => (images.current[11] = el)}
+                    />
                   </div>
-                  <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/images_baptism/baptism3.jpg" className="img-fluid" alt="Baptism" data-bs-image="./images/baptism2.jpg" />
-                  </div>
-                  <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/images_baptism/baptism4.jpg" className="img-fluid" alt="Baptism" data-bs-image="./images/baptism2.jpg" />
-                  </div>
-                  <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/images_baptism/baptism5.jpg" className="img-fluid" alt="Baptism" data-bs-image="./images/baptism2.jpg" />
-                  </div>
-                  <div className="col-md-6 d-flex justify-content-center py-2">
-                    <img src="./images/images_baptism/baptism6.jpg" className="img-fluid" alt="Baptism" data-bs-image="./images/baptism2.jpg" />
-                  </div>
+                 
                 </div>
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </div>
   );
 };
 
-export {Media};
+export { Media };
