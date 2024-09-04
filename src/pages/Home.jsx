b62cc63ecfa4273ec280b900b24d34d2d19511f8
IgorLipskiy <igoryurievich1@gmail.com>
2024-09-04 21:22:11 +0200

    Откат, нет необходимости в этом функционале. Применится в галерее

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Home.css';
 import './adition.css';
-import { storage, ref, getDownloadURL } from '../firebase'; // Импорт из firebase.js
-
 const Home = () => {
-  const [imageUrl, setImageUrl] = useState('');
-
-  useEffect(() => {
-    const imageRef = ref(storage, 'banners/media134.png');
-
-    getDownloadURL(imageRef)
-      .then((url) => {
-        setImageUrl(url);
-      })
-      .catch((error) => {
-        console.error('Error fetching image URL:', error);
-      });
-  }, []);
-
   return (
     <div className="corpo">
+
+      
       <div className="image-container" id="banner">
         <div className="text-overlay">
           <h1 id="titul">Церковь ЕХБ <br />"Свет Жизни"</h1>
         </div>
-        {imageUrl && <img src={imageUrl} alt="Описание изображения" />}
+        <img src="/images/media134.png" alt="Описание изображения" />
       </div>
     
       <div className="container-md py-4">
