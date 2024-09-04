ce1b66fa35c4637d22127f105d7b0bc90ae6a08a
IgorLipskiy <igoryurievich1@gmail.com>
2024-09-05 00:31:13 +0200

    Придется пожертвовать протоколам безопасности во имя успешного деплоя в netlify

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,18 @@
 // src/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getStorage, ref, listAll, getDownloadURL } from 'firebase/storage';
-import storageConfig from './config/storageConfig'; 
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_API_KEY,
     authDomain: import.meta.env.VITE_AUTH_DOMAIN,
     projectId: import.meta.env.VITE_PROJECT_ID,
     messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID
+    appId: import.meta.env.VITE_APP_ID,
+    storageBucket: 'baptistchurch-61b70.appspot.com'  // Добавляем URL бакета сюда
 };
 
 const app = initializeApp(firebaseConfig);
 
-// Используйте URL бакета из конфигурационного файла
-const storage = getStorage(app, storageConfig.bucketUrl);
+const storage = getStorage(app);
 
-export { storage, ref, listAll, getDownloadURL }; 
+export { storage, ref, listAll, getDownloadURL };
