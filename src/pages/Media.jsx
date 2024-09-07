44f6b6824b9f695b45eaf8cb8456b0de8fd76d9b
IgorLipskiy <igoryurievich1@gmail.com>
2024-09-07 20:32:30 +0200

    Исправление разметки галерея

--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { storage, ref, listAll, getDownloadURL } from '../firebase';
-import LazyLoad from 'react-lazyload'; // Импортируем LazyLoad
+import LazyLoad from 'react-lazyload';
 import PhotoModal from './photomodal';
 import './gallery.css';
 
@@ -36,26 +36,22 @@ const Media = () => {
 
   return (
     <div className="corpo">
-    
-       <h1 id="titull" className="container py-4 text-center">Видео</h1>
-       <div className="row px-4">
-                  <div className="col-md-6 py-2">
-                    <div className="ratio ratio-16x9">
-                      <iframe width="560" height="315" src="https://www.youtube.com/embed/7p7PSTqobiQ?si=8ucnmj0OyrC7SeSG" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                    </div>
-                  </div>
-                  <div className="col-md-6 py-2">
-                    <div className="ratio ratio-16x9">
-                      <iframe width="560" height="315" src="https://www.youtube.com/embed/giOXGaxqCqo?si=pA0ZUwtHl2zeNK52" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                    </div>
-                  </div>
-                </div>
-
-    
-        <h1 id="titull" className="container py-4 text-center">Фотографии</h1>
-        
-    
-
+      <h1 id="titull" className="container py-4 text-center">Видео</h1>
+      <div className="container">
+        <div className="row px-4">
+          <div className="col-md-6 py-2">
+            <div className="ratio ratio-16x9">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/7p7PSTqobiQ?si=8ucnmj0OyrC7SeSG" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            </div>
+          </div>
+          <div className="col-md-6 py-2">
+            <div className="ratio ratio-16x9">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/giOXGaxqCqo?si=pA0ZUwtHl2zeNK52" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+      <h1 id="titull" className="container py-4 text-center">Фотографии</h1>
       <div className="container">
         <div className="row">
           {imageUrls.map((url, index) => (
@@ -69,7 +65,6 @@ const Media = () => {
           ))}
         </div>
       </div>
-
       <PhotoModal isOpen={modalOpen} currentIndex={currentImageIndex} images={imageUrls} onClose={closeModal} />
     </div>
   );
